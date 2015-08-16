8e470acd7fd9cd9a609a2c4d3874064eb05513b6
Jon Haddad <jon@jonhaddad.com>
2015-08-16 10:49:02 -0700

    kernel file

--- cassandra_jupyter_kernel/cassandra.py
+++ cassandra_jupyter_kernel/cassandra.py
@@ -1,24 +1,27 @@
-from IPython.kernel.zmq.kernelbase import Kernel
+from ipykernel.kernelbase import Kernel
 
 class CassandraKernel(Kernel):
     implementation = 'Cassandra'
     implementation_version = '1.0'
     language = 'cql'
     language_version = '0.1'
-    language_info = {'mimetype': 'text/plain'}
+    language_info = {'mimetype': 'text/plain', "name":"cql"}
     banner = "Cassandra CQL 3 Kernel"
+
+    cluster = None
 
     def do_execute(self, code, silent,
                    store_history=True, user_expressions=None,
                    allow_stdin=False):
-                   
+
         # check to see if it's a login
         # if we don't have a connection, complain
         # if it's a query, execute
         # it could be a histogram, or line graph
 
+        print code
         if not silent:
-            stream_content = {'name': 'stdout', 'text': code}
+            stream_content = {'name': 'stdout', 'text': "eat shit"}
             self.send_response(self.iopub_socket, 'stream', stream_content)
 
         return {'status': 'ok',
@@ -28,6 +31,7 @@ class CassandraKernel(Kernel):
                 'user_expressions': {},
                }
 
+
 if __name__ == '__main__':
-    from IPython.kernel.zmq.kernelapp import IPKernelApp
+    from ipykernel.kernelapp import IPKernelApp
     IPKernelApp.launch_instance(kernel_class=CassandraKernel)
