6125ef88923878764ba0ef5e97f4852fc992829a
Jon Haddad <jon@jonhaddad.com>
2015-09-01 18:41:44 -0700

    display stuff

--- cassandra_jupyter_kernel/kernel.py
+++ cassandra_jupyter_kernel/kernel.py
@@ -3,13 +3,22 @@ from ipykernel.kernelbase import Kernel
 import sys
 from cassandra.query import dict_factory
 
+import matplotlib
+# matplotlib.style.use('ggplot')
+from base64 import b64encode
+
+from StringIO import StringIO
+
 print sys.executable
 print sys.argv
+
+from logging import warn
 
 from cassandra.cluster import Cluster
 from pandas import DataFrame
 
 connect = re.compile("%connect (.*)")
+line_graph = re.compile("%line (.*)")
 
 class CassandraKernel(Kernel):
     implementation = 'Cassandra'
@@ -27,14 +36,57 @@ class CassandraKernel(Kernel):
                    allow_stdin=False):
 
         # check to see if it's a %connect
-        if not self.cluster:
-            self.cluster = Cluster()
+        connect_str = connect.search(code)
+
+        if connect_str:
+            address = connect_str.groups()[0]
+            self.cluster = Cluster([address])
             self.cassandra_session = self.cluster.connect("test")
             self.cassandra_session.row_factory = dict_factory
 
+            self.send_response(self.iopub_socket, 'stream', {"name":"stdout", "text":"connected"})
+            return {"status": "ok",
+                    'execution_count': self.execution_count,
+                    'payload': [],
+                    'user_expressions': {} }
+
+        if not self.cluster:
+            warn("No connection")
+            return {"status": "ok",
+                    'execution_count': self.execution_count,
+                    'payload': [],
+                    'user_expressions': {}  }
         # if we don't have a connection, complain
         # if it's a query, execute
         # it could be a histogram, or line graph
+
+
+        lg = line_graph.search(code)
+
+        if lg:
+            result = self.cassandra_session.execute(lg.groups()[0])
+            df = DataFrame(result)
+            p = df.plot(kind='line')
+            fig = p.get_figure()
+            s = StringIO()
+            fig.savefig(s, format="png")
+            s.seek(0)
+
+            # do line graph stuff
+        
+            stream_content = {"name":"stdout",
+                              "data": {
+                                "image/png": b64encode(s.buf)
+                              },
+                              "metadata": {}
+                              }
+            self.send_response(self.iopub_socket, 'display_data', stream_content)
+            return {'status': 'ok',
+                    # The base class increments the execution count
+                    'execution_count': self.execution_count,
+                    'payload': [],
+                    'user_expressions': {},
+                   }
 
         result = self.cassandra_session.execute(code)
         df = DataFrame(result)
